08d0c5ca56db9a6ebc843549ef3da14e39186d62
javierIA <javier.flores@ia.center>
2022-02-18 17:25:41 -0700

    lol

--- test.py
+++ test.py
@@ -1,148 +1,98 @@
-from calendar import c
 from pathlib import Path
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel,QFileDialog,QDialog,QComboBox,QMessageBox,QCheckBox
-from PySide6.QtGui import QIcon, QPixmap, QFont, QPalette
-from PySide6.QtCore import Qt,Slot,QTextStream,QFile
+from PySide6.QtWidgets import QApplication,QHBoxLayout,QWidget, QPushButton, QLabel,QFileDialog,QDialog,QComboBox,QMessageBox,QCheckBox,QSlider
+from PySide6.QtGui import QIcon, QPixmap, QFont
+from PySide6.QtCore import Qt
 import qdarkstyle
-import subprocess
 import sys
 import os
 from helpers import tools 
-from pathlib import Path
+from pathlib import Path , PureWindowsPath
+from ui import initui
+import shutil
 class Window(QWidget):
     pathvideo=None
     pathaudio=None
     musicnumber=1
     removeNoise=False
+  
     def __init__(self):
         super().__init__()
-              
-        self.title = "Video Mixer"
-        self.setGeometry(100, 100, 300, 300)
-        self.setWindowTitle(self.title)
-
-        lbl1=QLabel(self)
-        lbl1.setText("Open Video:")
-        lbl1.setGeometry(50,50,100,30)    
-        lbl1.setFont(QFont("Arial",12))
-        lbl1.setAlignment(Qt.AlignLeft)
-
-        btn1=QPushButton(self)
-        btn1.setText("Open")
-        btn1.setGeometry(150,50,100,30)
-        btn1.setFont(QFont("Arial",12))
-        btn1.clicked.connect(self.openvideo)
-
-        lbl2=QLabel(self)
-        lbl2.setText("Open Audio:")
-        lbl2.setGeometry(50,100,100,30)
-        lbl2.setFont(QFont("Arial",12))
-        lbl2.setAlignment(Qt.AlignLeft)
-
-        btn2=QPushButton(self)
-        btn2.setText("Open")
-        btn2.setGeometry(150,100,100,30)
-        btn2.setFont(QFont("Arial",12))
-        btn2.clicked.connect(self.openaudio)
-
-
-        lbl3=QLabel(self)
-        lbl3.setText("Music:")
-        lbl3.setGeometry(50,200,100,30)
-        lbl3.setFont(QFont("Arial",12))
-        lbl3.setAlignment(Qt.AlignLeft)
-       
-        comb1=QComboBox(self)
-        comb1.setGeometry(150,200,100,30)
-        comb1.setFont(QFont("Arial",12))
-        comb1.addItems(["1","2","3","4","5","6"]) 
-        comb1.currentTextChanged.connect(self.change) 
-
-        lblrun=QLabel(self)
-        lblrun.setText("Run")
-        lblrun.setGeometry(50,150,100,30)
-        lblrun.setFont(QFont("Arial",12))
-        lblrun.setAlignment(Qt.AlignLeft)
-        #enviar el btnrun al final
+        initui(self)
         
-        btnrun=QPushButton(self)
-        btnrun.setText("Run")
-        btnrun.setGeometry(150,150,100,30)
-        btnrun.setFont(QFont("Arial",12))
-        btnrun.setFlat(True)
-        btnrun.clicked.connect(self.run)
-
-        lbl4=QLabel(self)
-        lbl4.setText("Is noise:")
-        lbl4.setGeometry(50,250,100,30)
-        lbl4.setFont(QFont("Arial",12))
-        lbl4.setAlignment(Qt.AlignLeft)
-
-        checkbox=QCheckBox(self)
-        checkbox.setGeometry(150,250,100,30)
-        checkbox.setFont(QFont("Arial",12))
-        checkbox.setChecked(False)
-        checkbox.stateChanged.connect(self.changeNoise)
-        checkbox.setText("NoiseREMOVE")
-
     
-
+    def changevolumenvoice(self,value):
+        self.lbl5.setText(f"Voice : {value} DB")
+        print(value)
+    
+    def changevolumenmusic(self,value):
+        self.lbl6.setText(f"Music : {value} DB")
+        print(value)
+   
     def changeNoise(self,CheckState):
        Window.removeNoise=CheckState
+   
     def change(self,text):
         Window.musicnumber=text
         print(Window.musicnumber)
+   
     def openvideo(self):
-
         fileName = QFileDialog.getOpenFileName(self,
         "Open video", "..", "Video Files (*.mp4)")
         Window.pathvideo=fileName[0]
+    
     def openaudio(self):
-
         fileName = QFileDialog.getOpenFileName(self,
         "Open audio", "..", "Audio Files (*.mp3)")
         Window.pathaudio=fileName[0]
+    
     def run(self):
-        video_path=Window.pathvideo
-        audio_path=Window.pathaudio
+        
+        if Window.pathvideo!=None or Window.pathaudio!=None:
+            os.makedirs("tmp", exist_ok=True)
+            shutil.copy(Window.pathaudio, "tmp/audio.mp3")
+            video_path=Window.pathvideo
+            audio_path=str(PureWindowsPath(str(os.getcwd())+"/tmp/audio.mp3"))
      
-        if  video_path != None or audio_path != None :
             output_path =  QFileDialog.getExistingDirectory(self,"SaveFIle", "..")
             try:
                 if Window.removeNoise:
                     print("removendo ruido")
-                    tools.removeNoise(video_path,output_path)
+                    tools.removeNoise(audio_path)
                 
-                tools.normalizeAudio(audio_path,numbermusic=int(Window.musicnumber))
+                tools.normalizeAudio(audio_path,numbermusic=int(Window.musicnumber),volumenvoice=float(self.voicevolumen.value()),volumenmusic=int(self.slider2.value()))  
                 filename = Path(video_path).name
                 os.system("ffmpeg -i  "+video_path+" -i "+audio_path+" -map 0:v -map 1:a -c:v copy -shortest  "+output_path+"/"+str(filename))
 
              
                # subprocess.call(f"ffmpeg -i  {video_path} -i  {audio_path} -map 0:v -map 1:a -c:v copy -shortest  "+output_path+"/"+str(filename))
-                dlg = QDialog(self)
-                dlg.setWindowTitle("Success")
-                dlg.setGeometry(100, 100, 300, 300)
-                
+                msgBox = QMessageBox(self)
+                msgBox.setText("Se guardo en ."+output_path+"/"+str(filename)) 
+                msgBox.setWindowTitle("Error")
+                msgBox.exec()
+
             
             except IndexError as e:
-            
-               dialogo=QMessageBox.critical(self, "Diálogo de error", e)
-               dialogo.setWindowTitle("Error")
-               dialogo.show()
+                msgBox=QMessageBox()
+                msgBox=QMessageBox.critical(self, "Diálogo de error", str(e))
+                msgBox.exec()
                
-        else:
-                dialogo=QMessageBox.critical(self, "Diálogo de error", "No se ha seleccionado ningún archivo")
-                dialogo.setWindowTitle("Error")
-                dialogo.show()
-           
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    window = Window()
-    window.show()
+        else:   
+                msgBox=QMessageBox()
+                msgBox=QMessageBox.critical(self, "Diálogo de error", "No se ha seleccionado ningún archivo")
+                msgBox.show()
+        os.remove("tmp/audio.mp3")
+        shutil.rmtree('tmp')
 
-    try:
-        sys.exit(app.exec_())
-    except SystemExit:
-        print('Closing Window...')
+           
+          
+def main():
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_from_environment())
+    ex = Window()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
     
